83932b694ec90524cf7bdc4f214363240557c9d5
QINXIN <zhangqx@udel.edu>
2018-05-05 12:52:01 -0400

    master

---
+++ resume/app/src/main/java/com/project/qinxin/resume/util/DateUtils.java
@@ -0,0 +1,27 @@
+package com.project.qinxin.resume.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by QINXIN on 3/25/18.
+ */
+
+public class DateUtils {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("MM/yyyy", Locale.getDefault());
+
+    public static String dateToString(Date date) {
+        return sdf.format(date);
+    }
+
+    public static Date stringToDate(String dateString) {
+        try {
+            return sdf.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return new Date(0);
+        }
+    }
+}
